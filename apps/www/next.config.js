60509457cd31a7988bc15601e4c2acf4329bf13b
Wesbitty Inc <wesbitty@outlook.com>
2022-12-30 02:01:59 +0000

    Script

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -5,11 +5,13 @@ module.exports = withWesjet({
   images: {
     domains: ['pbs.twimg.com', 'avatars.githubusercontent.com', 'github.com', 'i.imgur.com'],
   },
-  headers: async () => [{
-    source: '/:path*',
-    headers: [
-      { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
-      { key: 'Cross-Origin-Embedder-Policy', value: 'same-origin' },
-    ],
-  }],
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+        { key: 'Cross-Origin-Embedder-Policy', value: 'same-origin' },
+      ],
+    },
+  ],
 })
