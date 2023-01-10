32ec60eb7fedc9db15324927dcc9703d3c8e080f
Wesbitty Inc <115786374+wesbitty@users.noreply.github.com>
2023-01-10 12:33:36 -0600

    Update _document.tsx

--- apps/www/pages/_document.tsx
+++ apps/www/pages/_document.tsx
@@ -1,9 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Metadata from '../@wesbitty/utils/schemas/Metadada'
+
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en-US" className="scroll-padding">
+      <Html lang="{Metadata.Language}" className="scroll-padding">
         <Head>
           <script async src="https://platform.twitter.com/widgets.js" />
         </Head>
