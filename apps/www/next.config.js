95a596ad2e040cb08d6dc8645f439f6f152a62eb
Wesbitty Inc <wesbitty@outlook.com>
2023-01-20 00:31:42 +0000

    DockerFile Output Traces

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -2,6 +2,18 @@
 const { withWesjet } = require('wesjet-nextjs-plugin')
 
 module.exports = withWesjet({
-  distDir: 'build',
   output: 'standalone',
+  images: {
+    domains: ['pbs.twimg.com', 'avatars.githubusercontent.com', 'i.imgur.com', 'user-images.githubusercontent.com', 'colab.research.google.com', 'avatars.githubusercontent.com', 'avatars.githubusercontent.com', 'github.com'],
+  },
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        { key: 'Strict-Transport-Security', value: '' },
+        { key: 'X-Robots-Tag', value: 'all' },
+        { key: 'X-Frame-Options', value: 'DENY' },
+      ],
+    },
+  ],
 })
