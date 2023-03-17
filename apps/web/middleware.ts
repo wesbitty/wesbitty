0cfa4b3c02921032998abe687c9b339070ac786b
Wesbitty Inc <wesbitty@outlook.com>
2023-03-17 16:43:31 +0000

    update deps

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -17,16 +17,11 @@ export const config = {
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
-  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  const hostname = req.headers.get("host") || "demo.vercel.pub";
+  // Get hostname of request (e.g. demo.wesbitty.org, demo.localhost:3000)
+  const hostname = req.headers.get("host") || "demo.wesbitty.org";
 
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
-
-  // Only for demo purposes - remove this if you want to use your root domain as the landing page
-  if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
-    return NextResponse.redirect("https://demo.vercel.pub");
-  }
 
   /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
@@ -35,7 +30,7 @@ export default async function middleware(req: NextRequest) {
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-          .replace(`.vercel.pub`, "")
+          .replace(`.bitty.vercel.app`, "")
           .replace(`.wesbitty.org`, "")
       : hostname.replace(`.localhost:3000`, "");
 
