3070b3bf76c0028783f30f2d25330fda019e6e6d
wesbitty <wesbitty@outlook.com>
2023-03-16 22:57:10 -0700

    midddleware

--- apps/platform/middleware.ts
+++ apps/platform/middleware.ts
@@ -22,16 +22,21 @@ export default async function middleware(req: NextRequest) {
 
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
+  // Only for demo purposes - remove this if you want to use your root domain as the landing page
+  if (hostname === "wesbitty.org" || hostname === "platforms.vercel.app") {
+    return NextResponse.redirect("https://demo.vercel.pub");
+  }
 
-  /*  You have to replace ".wesbitty.org" with your own domain if you deploy this example under your domain.
+  /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
-      in this case, our team slug is "platformize", thus *.bitty.vercel.app works. Do note that you'll
-      still need to add "*.bitty.vercel.app" as a wildcard domain on your Vercel dashboard. */
+      in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
+      still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-          .replace(`.localhost:3000`, "")
-      : hostname.replace(`.wesbitty.org`, "");
+          .replace(`.wesbitty.org`, "")
+          .replace(`.bitty.vercel.app`, "")
+      : hostname.replace(`.localhost:3000`, "");
 
   // rewrites for app pages
   if (currentHost == "app") {
@@ -48,7 +53,7 @@ export default async function middleware(req: NextRequest) {
   }
 
   // rewrite root application to `/home` folder
-  if (hostname === "localhost:3000" || hostname === "wesbitty.org") {
+  if (hostname === "localhost:3000" || hostname === "bitty.vercel.app") {
     return NextResponse.rewrite(new URL(`/home${path}`, req.url));
   }
 
