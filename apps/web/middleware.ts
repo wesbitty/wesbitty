a284e5105712490c3e25c8a9dcd6f02bd0e2dbab
wesbitty <wesbitty@outlook.com>
2023-03-18 12:56:52 -0700

    Update middleware.ts

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,13 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
   matcher: [
-    /*
-     * Match all paths except for:
-     * 1. /api routes
-     * 2. /_next (Next.js internals)
-     * 3. /examples (inside /public)
-     * 4. all root files inside /public (e.g. /favicon.ico)
-     */
     "/((?!api/|_next/|_static/|examples/|[\\w-]+\\.\\w+).*)",
   ],
 };
@@ -17,24 +10,17 @@ export const config = {
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
-  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
   const hostname = req.headers.get("host") || "platforms.vercel.app";
 
-  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
 
-  /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
-      You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
-      in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
-      still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
        ? hostname
-          .replace(`.localhost:3000`, "")
+          .replace(`.wesbitty.org`, "")
           .replace(`.bitty.vercel.app`, "")
-        :hostname.replace(`.wesbitty.org`, "");
+        :hostname.replace(`.localhost:3000`, "");
 
-  // rewrites for app pages
   if (currentHost == "app") {
     if (
       url.pathname === "/login" &&
@@ -49,12 +35,10 @@ export default async function middleware(req: NextRequest) {
     return NextResponse.rewrite(url);
   }
 
-  // rewrite root application to `/home` folder
-  if (hostname === "wesbitty.org" || hostname === "bitty.vercel.app") {
+  if (hostname === "localhost:3000" || hostname === "bitty.vercel.app") {
     return NextResponse.rewrite(new URL(`/home${path}`, req.url));
   }
 
-  // rewrite everything else to `/_sites/[site] dynamic route
   return NextResponse.rewrite(
     new URL(`/_sites/${currentHost}${path}`, req.url)
   );
