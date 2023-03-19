7806bb3d5ca0bc7f66ee49217b68fbd8dc0be5d9
Wesbitty Inc <wesbitty@outlook.com>
2023-03-19 20:55:40 +0000

    Format: Prettier

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,20 +1,20 @@
 module.exports = {
-  extends: ["next", "react-hooks", "prettier"],
+  extends: ['next', 'react-hooks', 'prettier'],
   settings: {
     next: {
-      rootDir: ["apps/*/", "packages/*/"],
+      rootDir: ['apps/*/', 'packages/*/'],
     },
   },
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "@next/next/no-page-custom-font": "off",
-    "react/jsx-key": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "react/no-unescaped-entities": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    '@next/next/no-page-custom-font': 'off',
+    'react/jsx-key': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/no-unescaped-entities': 'off',
   },
   parserOptions: {
     babelOptions: {
-      presets: [require.resolve("next/babel")],
+      presets: [require.resolve('next/babel')],
     },
   },
-};
+}
