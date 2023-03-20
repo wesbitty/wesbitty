f0d7aba70eb9579296d30825973be992e4806959
wesbitty <wesbitty@outlook.com>
2023-03-20 09:56:53 -0700

    Feature

--- apps/web/components/Widget/Header/index.tsx
+++ apps/web/components/Widget/Header/index.tsx
@@ -184,8 +184,8 @@ const Header = () => {
       <div className="sticky top-0 z-50">
         <nav className="bg-white dark:bg-gray-800 border-b dark:border-gray-600">
           {/* <div className="lg:container mx-auto relative flex justify-between h-16 lg:px-10 xl:px-0"> */}
-          <div className="container mx-auto relative flex justify-between h-16 lg:px-16 xl:px-20">
-            <div className="flex items-center justify-center sm:items-stretch lg:justify-between">
+          <div className="relative mx-auto flex h-16 justify-between lg:container lg:px-16 xl:px-20">
+          <div className="flex flex-1 items-center justify-center sm:items-stretch lg:justify-between">
               <div className="flex items-center">
                 <div className="flex items-center space-x-2.5">
                   <Link href="/">
@@ -197,12 +197,12 @@ const Header = () => {
                   </Link>
                 </div>
                 <HamburgerButton toggleFlyOut={() => setOpen(true)} />
-                <div className="pl-4 hidden sm:ml-6 lg:flex sm:space-x-4">
+                <div className="hidden pl-8 sm:ml-6 sm:space-x-4 lg:flex">
                   <Link
                     href="/blog"
                     className={`
                     inline-flex items-center px-1 border-b-2 border-transparent text-sm font-medium
-                    text-gray-500 hover:text-gray-700 hover:border-gray-500 p-5
+                    text-gray-700 hover:text-gray-700 hover:border-gray-500 p-5
                     dark:text-dark-100 dark:hover:border-dark-100
                   `}
                   >
@@ -246,7 +246,7 @@ const Header = () => {
                     Sign In
                   </Button>
                 </Link>
-              </div>
+                </div>
             </div>
             {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"></div> */}
           </div>
@@ -262,7 +262,7 @@ const Header = () => {
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 translate-y-1"
           >
-            <div className="p-4 md:p-8 h-screen w-screen fixed bg-white transform overflow-y-scroll -inset-y-0 z-50 dark:bg-dark-700">
+          <div className="p-4 md:p-8 h-screen w-screen fixed bg-white transform overflow-y-scroll -inset-y-0 z-50 dark:bg-dark-700">
               <div className="absolute right-4 top-4 items-center justify-between">
                 <div className="-mr-2">
                   <button
