c91440cb8155702d312f36ea97f4e6119e138554
wesbitty <wesbitty@outlook.com>
2023-03-20 09:55:18 -0700

    Font

--- apps/web/styles/CustomFonts.ts
+++ apps/web/styles/CustomFonts.ts
@@ -1,21 +1,42 @@
-import localFont from '@next/font/local'
-import { Inter, Lora, Work_Sans } from '@next/font/google'
+import localFont from "next/font/local"
+import { Inter, Lora, Work_Sans } from "next/font/google"
 
 export const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
 })
+
 export const cal = localFont({
-  src: './CalSans-SemiBold.woff2',
+  src: [
+    {
+      path: './fonts/CustomFont-Book.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/CustomFont-Book.woff',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
   variable: '--font-cal',
-  weight: '600',
   display: 'swap',
 })
 
 export const calTitle = localFont({
-  src: './CalSans-SemiBold.woff2',
+  src: [
+    {
+      path: './fonts/CustomFont-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/CustomFont-Medium.woff',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
   variable: '--font-title',
-  weight: '600',
   display: 'swap',
 })
 export const lora = Lora({
