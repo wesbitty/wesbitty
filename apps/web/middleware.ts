ed2829c068bf764e611f1a8bfd2885d9d1f2ae74
wesbitty <wesbitty@outlook.com>
2023-03-20 18:08:40 -0700

    Middleware Nameserver issues

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,23 +3,15 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
   matcher: [
-    '/((?!api/|_next/|_static/|fonts/|brand/|images/|examples/|[\\w-]+\\.\\w+).*)',
+    '/((?!api/|_next/|_static/|brand/|images/|examples/|[\\w-]+\\.\\w+).*)',
   ],
 }
 
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl
-
-  // Get hostname of request (e.g. demo.wesbitty.org, demo.localhost:3000)
   const hostname = req.headers.get('host') || 'wesbitty.org'
-
-  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname
 
-  /*  You have to replace ".wesbitty.org" with your own domain if you deploy this example under your domain.
-      You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
-      in this case, our team slug is "platformize", thus *.bitty.vercel.app works. Do note that you'll
-      still need to add "*.bitty.vercel.app" as a wildcard domain on your Vercel dashboard. */
   const currentHost =
     process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
       ? hostname.replace(`.wesbitty.org`, '').replace(`.bitty.vercel.app`, '')
