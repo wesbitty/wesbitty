9d9d1bce5577605157b028e2c9a6940bf341c019
Wesbitty Inc <iamsharaff@gmail.com>
2023-03-23 08:06:02 +0000

    404 NotFound

--- apps/web/pages/404.tsx
+++ apps/web/pages/404.tsx
@@ -39,15 +39,15 @@ const NotFound = () => {
           ],
         }}
       />
-      <Default hideHeader>
+      <Default hideHeader hideFooter>
         <Section>
           <Container>
             <main className="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
               <div className="text-center">
-                <p className="flex items-center font-semibold text-5xl mb-4 text-indigo-600">
-                  404 <IconFrown size="140" type="secondary" />
+                <p className="flex items-center text-center font-semibold text-5xl mb-4 text-indigo-600">
+                  404 <IconFrown size="10"  type="secondary" />
                 </p>
-                <h1 className="mt-4 text3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                <h1 className="mt-4 text2xl font-bold tracking-tight text-gray-900 sm:text-5xl">
                   Page Not Found
                 </h1>
                 <p className="mt-6 text-base leading-7 text-gray-600">
