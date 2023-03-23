7c4f13c5fe3eed1d459dcbb76961960bd7e2cc57
wesbitty <wesbitty@outlook.com>
2023-03-22 20:25:45 -0700

    404

--- apps/web/pages/404.tsx
+++ apps/web/pages/404.tsx
@@ -41,19 +41,29 @@ const NotFound = () => {
         }}
       />
       <ErrorWidget>
-        <div className="errorWrap">
-          <h3>
-            404{' '}
-            <span>
-              <IconFrown size="140" type="secondary" />
-            </span>
-          </h3>
-          <h4>There&#39;s nothing here!</h4>
-          <p>Sorry, the page you were looking for in this site does not exist.</p>
-          <Link className="homepage" href="/">
-            Go to Home Page
-          </Link>
-        </div>
+        <main className="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
+          <div className="text-center">
+            <p className="font-semibold text-5xl mb-4 text-indigo-600">
+              404{' '}
+              <span>
+                <IconFrown size="140" type="secondary" />
+              </span>
+            </p>
+            <h1 className="mt-4 text-2xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page Not Found</h1>
+            <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <Link
+                href="/"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Go back home
+              </Link>
+              <Link href="/support" className="text-sm font-semibold text-gray-900">
+                Contact support <span aria-hidden="true">&rarr;</span>
+              </Link>
+            </div>
+          </div>
+        </main>
       </ErrorWidget>
     </>
   )
