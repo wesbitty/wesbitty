fa7ad819c34d0cca548b4c05c7567c7b28ca12a1
Dimeji SR <iamsharaff@gmail.com>
2023-04-22 11:35:00 +0000

    eslintPlugin

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: ['plugin:wesjet/vital'],
+  extends: ['next/core-web-vitals', 'react'],
+  rules: {
+    'react/jsx-key': 'off',
+    'react-hooks/rules-of-hooks': 'off',
+    '@next/next/no-img-element': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
+  },
 }
