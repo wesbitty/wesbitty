719a6c2fdb17ccae6ee9bddd2274f4e0cbb9ba7a
Bitty <bobbypetter345@gmail.com>
2023-05-01 06:25:54 +0000

    wesjdtConfig

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,11 @@
 const { wesjetConfig } = require('wesjet/next')
 
 module.exports = wesjetConfig({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     domains: [
       'res.cloudinary.com',
       'abs.twimg.com',
+      'i.imgur.com',
       'pbs.twimg.com',
       'avatars.githubusercontent.com',
     ],
