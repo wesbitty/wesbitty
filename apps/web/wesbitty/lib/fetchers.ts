576734dc3628ae853b560369121740bcdcd96c01
IAm Nicholax <bobbypetter345@gmail.com>
2023-05-05 04:14:14 +0000

    fetchers

--- apps/web/wesbitty/lib/fetchers.ts
+++ apps/web/wesbitty/lib/fetchers.ts
@@ -1,5 +1,5 @@
 import { cache } from 'react'
-import type { _SiteData } from 'types'
+import type { _SiteData } from '~/types'
 import prisma from '~/lib/prisma'
 import { remark } from 'remark'
 import { serialize } from 'next-mdx-remote/serialize'
