f1fd90a15658ad7f46d410277cd1799a1f50e5eb
IAmNicholax <112731405+iamnicholax@users.noreply.github.com>
2023-05-05 11:13:35 -0500

    Update next.config.js
    Signed-off-by: IAmNicholax <112731405+iamnicholax@users.noreply.github.com>

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,19 +1,6 @@
 const { wesjetConfig } = require('wesjet/next')
 
 module.exports = wesjetConfig({
-  webpack: (config, { nextRuntime }) => {
-    // Undocumented property of next 12.
-    if (nextRuntime !== 'nodejs') return config
-    return {
-      ...config,
-      entry() {
-        return config.entry().then((entry) => ({
-          ...entry,
-          cli: path.resolve(process.cwd(), 'lib/cli.ts'),
-        }))
-      },
-    }
-  },
   images: {
     domains: [
       'res.cloudinary.com',
