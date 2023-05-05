75be630a30279ac2b832eaaa815dfccb52a2df6d
IAm Nicholax <bobbypetter345@gmail.com>
2023-05-05 20:53:07 +0000

    Moved to Root

---
+++ apps/web/lib/prisma.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const prisma = global.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV === 'development') global.prisma = prisma
+
+export default prisma
