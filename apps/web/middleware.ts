1d9bb98342db26b89229b4b08112b85cee3733f0
IAm Nicholax <bobbypetter345@gmail.com>
2023-05-05 07:00:51 +0000

    next@13.2.4

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,20 +3,43 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
   matcher: [
-    '/((?!api/|_next/|_static/|brand/|images/|examples/|[\\w-]+\\.\\w+).*)',
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /examples (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
+     */
+    '/((?!api/|_next/|_static/|examples/|brand/|images/|[\\w-]+\\.\\w+).*)',
   ],
 }
 
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl
-  const hostname = req.headers.get('host') || 'wesbitty.org'
+
+  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
+  const hostname = req.headers.get('host') || 'demo.wesbitty.org'
+
+  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname
 
+  // Only for demo purposes - remove this if you want to use your root domain as the landing page
+  if (hostname === 'wesbitty.org' || hostname === 'platforms.vercel.app') {
+    return NextResponse.redirect('https://demo.wesbitty.org')
+  }
+
+  /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
+      You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
+      in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
+      still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
   const currentHost =
     process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
-      ? hostname.replace(`.wesbitty.org`, '')
+      ? hostname
+          .replace(`.wesbitty.org`, '')
+          .replace(`.wesbittyv2.vercel.app`, '')
       : hostname.replace(`.localhost:3000`, '')
 
+  // rewrites for app pages
   if (currentHost == 'app') {
     if (
       url.pathname === '/login' &&
@@ -31,9 +54,11 @@ export default async function middleware(req: NextRequest) {
     return NextResponse.rewrite(url)
   }
 
-  if (hostname === 'localhost:3000' || hostname === 'wesbitty.org') {
+  // rewrite root application to `/home` folder
+  if (hostname === 'localhost:3000' || hostname === 'wesbittyv2.vercel.app') {
     return NextResponse.rewrite(new URL(`${path}`, req.url))
   }
 
+  // rewrite everything else to `/_sites/[site] dynamic route
   return NextResponse.rewrite(new URL(`/_sites/${currentHost}${path}`, req.url))
 }
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,19 @@
 const { wesjetConfig } = require('wesjet/next')
 
 module.exports = wesjetConfig({
+  webpack: (config, { nextRuntime }) => {
+    // Undocumented property of next 12.
+    if (nextRuntime !== 'nodejs') return config
+    return {
+      ...config,
+      entry() {
+        return config.entry().then((entry) => ({
+          ...entry,
+          cli: path.resolve(process.cwd(), 'lib/cli.ts'),
+        }))
+      },
+    }
+  },
   images: {
     domains: [
       'res.cloudinary.com',
