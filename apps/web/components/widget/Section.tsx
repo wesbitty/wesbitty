8faacd6c921061cb3c42e31649b124be37dfca5d
IAmNicholax <112731405+iamnicholax@users.noreply.github.com>
2023-05-10 23:36:15 -0500

    Section Widget
    Signed-off-by: IAmNicholax <112731405+iamnicholax@users.noreply.github.com>

--- apps/web/components/widget/Section.tsx
+++ apps/web/components/widget/Section.tsx
@@ -8,8 +8,11 @@ interface Props {
 
 export const Section = ({ children, className }: Props) => (
   <div
-    className={`container mx-auto px-6 lg:px-16 xl:px-20 relative py-16 sm:py-18 md:py-24 lg:py-24 ${className}`}
+    className={classNames(
+      `sm:py-18 container relative mx-auto px-6 py-16 md:py-24 lg:px-16 lg:py-24 xl:px-20`,
+      className
+    )}
   >
     {children}
-  </div>
+</div>
 )
