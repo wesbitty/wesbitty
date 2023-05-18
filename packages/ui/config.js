822b75c36c001d87b360b67ac8dc1eb3cd050e4d
IAm Nicholax <bobbypetter345@gmail.com>
2023-05-18 03:35:00 +0000

    theme

--- packages/ui/config.js
+++ packages/ui/config.js
@@ -463,7 +463,7 @@ function arrayMergeFn(destinationArray, sourceArray) {
 }
 
 /**
- * Merge Wesjet CSS and Tailwind CSS configurations
+ * Merge Wesbitty and Tailwind CSS configurations
  * @param {object} tailwindConfig - Tailwind config object
  * @return {object} new config object
  */
