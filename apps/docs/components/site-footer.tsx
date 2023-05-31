d290233a95120d2b1362a22667f0f2eb92ed6da7
IAmNicholax <112731405+iamnicholax@users.noreply.github.com>
2023-05-30 22:33:09 -0500

    Update site-footer.tsx
    Signed-off-by: IAmNicholax <112731405+iamnicholax@users.noreply.github.com>

--- apps/docs/components/site-footer.tsx
+++ apps/docs/components/site-footer.tsx
@@ -7,20 +7,7 @@ export function SiteFooter() {
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <div className="flex items-center gap-x-1.5">
-            <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-              © 2023{' '}
-              <a
-                href="/terms"
-                target="_blank"
-                rel="noreferrer"
-                className="font-medium underline underline-offset-4"
-              >
-                Wesbitty, Inc.
-              </a>
-            </p>
-          </div>
-
-          <div className="text-sm text-muted-foreground">
+          <p className="text-sm text-muted-foreground">
             <a href="/terms" className="font-medium">
               Terms
             </a>
@@ -37,7 +24,20 @@ export function SiteFooter() {
             <a href="/security" className="font-medium">
               Security
             </a>
+          </p>
           </div>
+          
+          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+              © 2023{' '}
+              <a
+                href="/terms"
+                target="_blank"
+                rel="noreferrer"
+                className="font-medium underline underline-offset-4"
+              >
+                Wesbitty, Inc.
+              </a>
+            </p>
         </div>
       </div>
     </footer>
