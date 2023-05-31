0ac37bdaf559abdca30599ffb03d3f4012290322
iam Nicholax <bobbypetter345@gmail.com>
2023-05-31 05:28:45 +0000

    runs-on

--- apps/web/components/PricingTableRow.tsx
+++ apps/web/components/PricingTableRow.tsx
@@ -1,45 +1,49 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { IconHelpCircle, IconWatch, Space, Typography } from 'ui'
+// @ts-ignore
 import ReactTooltip from 'react-tooltip'
 import ProductIcon from '~/components/Icons/product'
 
-const Chevron = (props: any) => (
-  <>
-    <svg
-      className={`h-5 w-5 text-green-500`}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fillRule="evenodd"
-        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-        clipRule="evenodd"
-      />
-    </svg>
-    <span className="sr-only">Included in {props.tier}</span>
-  </>
-)
+const Chevron = (props: any) => {
+  return (
+    <>
+      <svg
+        className={`h-5 w-5 text-green-500`}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+          clipRule="evenodd"
+        />
+      </svg>
+      <span className="sr-only">Included in {props.tier}</span>
+    </>
+  )
+}
 
-const Minus = (props: any) => (
-  <>
-    <svg
-      className="h-5 w-5 text-gray-400"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-      aria-hidden="true"
-    >
-      <path
-        fillRule="evenodd"
-        d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-        clipRule="evenodd"
-      />
-    </svg>
-    <span className="sr-only">Not included in {props.tier}</span>
-  </>
-)
+const Minus = (props: any) => {
+  return (
+    <>
+      <svg
+        className="h-5 w-5 text-gray-400"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          fillRule="evenodd"
+          d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+          clipRule="evenodd"
+        />
+      </svg>
+      <span className="sr-only">Not included in {props.tier}</span>
+    </>
+  )
+}
 
 export const PricingTableRowDesktop = (props: any) => {
   const category = props.category
